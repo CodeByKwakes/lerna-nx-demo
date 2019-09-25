2e182984a077c27dfaf9543c2e760aa401527506
Kwaku Prempeh <kwaku.prempeh@holition.com>
2019-09-25 11:24:12 +0100

    fix: new copnsole log

--- libs/weapon/src/lib/weapon.service.ts
+++ libs/weapon/src/lib/weapon.service.ts
@@ -7,7 +7,7 @@ export class WeaponService {
   constructor() {}
 
   getMessage() {
-    console.log('I am a weapon lol BREAKING CHANGE: new version');
+    console.log('I am a weapon lol ne fix');
   }
 
   getNewMsg() {
